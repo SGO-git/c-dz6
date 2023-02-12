5f08879aaf26aad31fe862c63da2cd2a7ec3b929
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-02-13 00:25:32 +0500

    add dz6

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+System.Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+
+if (Number >= 100)
+{
+    while (Number > 999)
+    {
+        Number = Number / 10;
+    }
+    res = Number % 10;
+    System.Console.WriteLine(res);
+}
+else
+{
+    System.Console.WriteLine("wrong number");
+}
